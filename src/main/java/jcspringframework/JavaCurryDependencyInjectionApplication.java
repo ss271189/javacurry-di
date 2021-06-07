c77c6c5b880bb1541a339794de063532867fb152
ss271189 <shashank2714@gmail.com>
2021-06-07 10:56:01 +0530

    Initial Commit

---
+++ src/main/java/jcspringframework/JavaCurryDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package jcspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCurryDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCurryDependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jcspringframework/JavaCurryDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package jcspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCurryDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
